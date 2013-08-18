83ccd5848facf6404a83462b8c5de319f145c9b5
Andrew Brampton <me@bramp.net>
2013-08-18 18:52:58 -0400

    Updated to use CodeMirror exclusively.

--- js/main.js
+++ js/main.js
@@ -1,71 +1,78 @@
+'use strict';
 
-angular.module('sms', [])
+// TODO consider _.memoize(function,
+
+function findMark(doc, pos2) {
+	var marks = doc.findMarksAt(pos2);
+	return _.find(marks, function(mark) {
+		var pos1 = mark.find().from;
+		if (eqPos(pos1, pos2))
+			return mark;
+	});
+}
+
+function eqPos(pos1, pos2) {
+	return pos1.line == pos2.line && pos1.ch == pos2.ch;
+}
+
+var app = angular.module('sms', ['ui.codemirror'])
+	.controller('SMSController', [ '$scope', function($scope) {
+		$scope.editorOptions = {
+			lineNumbers: false,
+			lineWrapping: true,
+			viewportMargin: Infinity,
+			onLoad: function(editor) {
+				var doc = editor.getDoc();
+				editor.on("change", function() {
+
+					var o = validate_gsm(editor.getValue());
+
+					var badChars = _.uniq(_.pluck(o.errors, 'char'));
+
+					if (badChars.length > 0) {
+						var regexp = new RegExp( "[" + badChars.join() + "]+" , "g");
+
+						// Runs everything in a single "transaction"
+						editor.operation(function() {
+							var cursor = editor.getSearchCursor(regexp);
+							while (cursor.findNext()) {
+								var mark = findMark(doc, cursor.from());
+								if (mark) {
+									if (!eqPos(mark.find().to, cursor.to())) {
+										mark.clear();
+										mark = undefined;
+									}
+								}
+
+								if (!mark) {
+									mark = doc.markText(
+										cursor.from(),
+										cursor.to(),
+										{className: 'sms-error', inclusiveLeft:false, inclusiveRight:false}
+									);
+								}
+							}
+
+						});
+					}
+				});
+			}
+	    };
+	}])
 	// Takes a model and outputs a simple counter
 	.directive('abSmsCount', function() {
 		return {
 			require: 'ngModel',
-			scope: {
-				model: '=ngModel'
-			},
-			template: '{{remaining}}/{{segments}}',
-			link: function (scope, elem, attrs, ngModel) {
-				scope.$watch("model", function (v) {
+			template: '<div class="alert alert-small" ng-class="{\'alert-success\' : !error, \'alert-error\' : error}">{{remaining}}/{{segments}}</div>',
+			link: function (scope, element, attrs, ngModel) {
+
+				scope.$watch(function () {
+					return ngModel.$modelValue;
+				}, function (v) {
 					var o = validate_gsm(v || "");
 					_.extend(scope, o);
+					scope.error = o.errors.length > 0;
 				});
 			}
 		};
 	})
-	// Generates a simple text editor that underlines invalid chars
-	.directive('abSmsBox', function() {
-		return {
-			restrict: 'E',
-			require: 'ngModel',
-			replace: true,
-			template: '<div contenteditable="true">One fine body…</div>',
-
-			link: function(scope, element, attrs, ngModel) {
-
-				// Specify how UI should be updated
-				ngModel.$render = function() {
-					element.html(ngModel.$viewValue || '');
-				};
-
-				// Listen for change events to enable binding
-				element.on('blur keyup change', function() {
-					scope.$apply(read);
-				});
-				read(); // initialize
-
-				// Write data to the model
-				function read() {
-					var html = element.html();
-					// When we clear the content editable the browser leaves a <br> behind
-					// If strip-br attribute is provided then we strip this out
-					if( attrs.stripBr && html == '<br>' ) {
-						html = '';
-					}
-					var text = htmlToText(html);
-					//var text = html;
-					ngModel.$setViewValue(text);
-				}
-
-/*
-				// view -> model
-				elm.on('keypress change input', function() {
-					scope.$apply( function() {
-						ctrl.$setViewValue(elm.text());
-					});
-				});
-
-				// model -> view
-				ctrl.$render = function() {
-					elm.html(ctrl.$viewValue);
-				};
-
-				// load init value from DOM
-				ctrl.$setViewValue(elm.text());
-*/
-			}
-		};
-	});
