94d388ff4cb65151a9e6a57e64292f416665ec3d
Andrew Brampton <me@bramp.net>
2013-08-19 20:22:49 -0400

    Finish.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 'use strict';
 
 // TODO consider _.memoize(function,
+RegExp.quote = function(str) {
+	return str.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+};
 
 function findMark(doc, pos2) {
 	var marks = doc.findMarksAt(pos2);
@@ -15,64 +18,100 @@ function eqPos(pos1, pos2) {
 	return pos1.line == pos2.line && pos1.ch == pos2.ch;
 }
 
-var app = angular.module('sms', ['ui.codemirror'])
+var app = angular.module('sms', ['ui.bootstrap', 'ui.codemirror'])
+	.constant('buttonConfig', {
+		activeClass: 'btn-primary'
+	})
 	.controller('SMSController', [ '$scope', function($scope) {
+
+		var encoding = $scope.encoding = "gsm_e";
+		$scope.encodings = SMS.encodings;
+
+		var editor, doc;
 		$scope.editorOptions = {
 			lineNumbers: false,
 			lineWrapping: true,
 			viewportMargin: Infinity,
-			onLoad: function(editor) {
-				var doc = editor.getDoc();
-				editor.on("change", function() {
-
-					var o = validate_gsm(editor.getValue());
-
-					var badChars = _.uniq(_.pluck(o.errors, 'char'));
-
-					if (badChars.length > 0) {
-						var regexp = new RegExp( "[" + badChars.join() + "]+" , "g");
-
-						// Runs everything in a single "transaction"
-						editor.operation(function() {
-							var cursor = editor.getSearchCursor(regexp);
-							while (cursor.findNext()) {
-								var mark = findMark(doc, cursor.from());
-								if (mark) {
-									if (!eqPos(mark.find().to, cursor.to())) {
-										mark.clear();
-										mark = undefined;
-									}
-								}
-
-								if (!mark) {
-									mark = doc.markText(
-										cursor.from(),
-										cursor.to(),
-										{className: 'sms-error', inclusiveLeft:false, inclusiveRight:false}
-									);
-								}
-							}
-
-						});
-					}
-				});
+			onLoad: function(e) {
+				editor = e;
+				doc = editor.getDoc();
 			}
 	    };
+
+	    $scope.$watchCollection('[message, encoding]', function () {
+	    	if (!editor || !doc)
+	    		return;
+
+	    	// If the encoding changes, clear old marks
+	    	if ($scope.encoding != encoding) {
+	    		editor.operation(function() {
+		    		_.invoke( doc.getAllMarks(), 'clear' );
+		    	});
+		    	encoding = $scope.encoding;
+	    	}
+
+			var validator = SMS.encodings[$scope.encoding].validator;
+			var o = validator($scope.message);
+
+			var badChars = _.uniq(_.pluck(o.errors, 'char'));
+			if (badChars.length > 0) {
+				var regexp = new RegExp( "[" + RegExp.quote(badChars.join()) + "]+" , "g");
+
+				// Runs everything in a single "transaction"
+				editor.operation(function() {
+					var cursor = editor.getSearchCursor(regexp);
+					while (cursor.findNext()) {
+						var mark = findMark(doc, cursor.from());
+						if (mark) {
+							if (!eqPos(mark.find().to, cursor.to())) {
+								mark.clear();
+								mark = undefined;
+							}
+						}
+
+						if (!mark) {
+							mark = doc.markText(
+								cursor.from(), cursor.to(), {
+									className: 'sms-error',
+									inclusiveLeft:false,
+									inclusiveRight:false
+								}
+							);
+						}
+					}
+
+				});
+			}
+	    });
+
 	}])
 	// Takes a model and outputs a simple counter
-	.directive('abSmsCount', function() {
+	.directive('smsCount', function() {
 		return {
 			require: 'ngModel',
-			template: '<div class="alert alert-small" ng-class="{\'alert-success\' : !error, \'alert-error\' : error}">{{remaining}}/{{segments}}</div>',
+			template: 
+				'<div class="alert alert-small" ng-class="{\'alert-success\' : !error, \'alert-error\' : error}">' +
+					'{{est_remaining}}/{{segments}}' +
+				'</div>',
+
 			link: function (scope, element, attrs, ngModel) {
+
+				function onChange() {
+					var encoding = attrs.smsEncoding ? attrs.smsEncoding : "gsm-e";
+					var value = ngModel.$modelValue || "";
+
+					var o = SMS.validate(encoding, value);
+					angular.extend(scope, o);
+
+					// TODO Change with validator
+					scope.error = o.errors.length > 0;
+				}
 
 				scope.$watch(function () {
 					return ngModel.$modelValue;
-				}, function (v) {
-					var o = validate_gsm(v || "");
-					_.extend(scope, o);
-					scope.error = o.errors.length > 0;
-				});
+				}, onChange);
+
+				attrs.$observe('smsEncoding', onChange);
 			}
 		};
 	})
--- js/smscount.js
+++ js/smscount.js
@@ -1,145 +1,162 @@
+(function(root) {
+    "use strict";
 
-    	// Assumes GSM 03.38 (with extension)
+    var SEGMENT_LENGTH = 140 * 8;
+    var UDH_LENGTH     =   6 * 8; // in bits
 
-    	// Based on the byte length, how many SMS Segments would this be
-    	// based on the concatenated SMS rules.
-    	// TODO Does this need to handle the case where a 7 bit char crosses a byte boundary
-    	// TODO Assumes 140 bytes. Canada caps at 136 characters
-	    function segments(bits, avg_char, errors) {
-	    	var MAX_BITS = 140 * 8;
-	    	var CONCAT_HEADER = 6 * 8; // in bits
+    /*
+    1   160
+    2   306
+    3   459
+    4 (Maximum) 612
+    */
 
-	    	var segments = 1;
-			if (bits > MAX_BITS) {
-				segments = Math.ceil(bits / (MAX_BITS - CONCAT_HEADER));
-			}	
+    // 3GPP TS 23.038 / GSM 03.38
+    var gsm = "@£$¥èéùìòÇ\nØø\rÅåΔ_ΦΓΛΩΠΨΣΘΞÆæßÉ !\"#¤%&'()*+,-./0123456789:;<=>?¡ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÑÜ§¿abcdefghijklmnopqrstuvwxyzäöñüà";
+    var gsm_extended = "^{}\[~]|€";
 
-			//var remaining = (((segments + 1) * MAX_BYTES) - bytes) / avg_char;
-			var bytes = Math.ceil(bits / 8);
-			var remaining = Math.floor((MAX_BITS - bits) / avg_char);
-			return {
-				bytes: bytes,
-				remaining: remaining, // Remaining chars
-				segments: segments,
-				errors: errors
-			};
-	    }
+    function is_gsm(char) {
+        return gsm.indexOf(char) !== -1;
+    }
 
-		// 3GPP TS 23.038 / GSM 03.38
-		var gsm = "@£$¥èéùìòÇ\nØø\rÅåΔ_ΦΓΛΩΠΨΣΘΞÆæßÉ !\"#¤%&'()*+,-./0123456789:;<=>?¡ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÑÜ§¿abcdefghijklmnopqrstuvwxyzäöñüà";
-		var gsm_extended = "^{}\[~]|€";
+    function is_gsm_extended(char) {
+        return gsm_extended.indexOf(char) !== -1;
+    }
 
-	    function is_gsm(char) {
-	    	return gsm.indexOf(char) !== -1;
-	    }
+    function is_ucs2(charCode) {
+        return charCode < 0xFFFF;
+    }
 
-	    function is_gsm_extended(char) {
-	    	return gsm_extended.indexOf(char) !== -1;
-	    }
+    function is_latin1(charCode) {
+        return charCode < 0xFF;
+    }
 
-	    function validate_gsm(text, extended) {
+	// Based on the byte length, how many SMS Segments would this be
+	// based on the concatenated SMS rules.
+	// TODO Does this need to handle the case where a 7 bit char crosses a byte boundary
+	// TODO Assumes 140 bytes. Canada caps at 136 characters
+	function segments(bits, avg_char, errors) {
 
-	    	var length = 0; // Length in bits
-    		var errors = [];
-    		extended = typeof extended !== 'undefined' ? extended : true;
+        var bytes = Math.ceil(bits / 8);
 
-    		for (var i = 0; i < text.length; i++) {
-    			var c = text.charAt(i);
-    			//console.log( text.charCodeAt(i) );
+        var next_segment = SEGMENT_LENGTH; // Boundary of next segment
+    	var segments = 1;
+		if (bits > SEGMENT_LENGTH) {
+            var segment_length_with_udh = SEGMENT_LENGTH - UDH_LENGTH;
+			segments = Math.ceil(bits / segment_length_with_udh);
+            next_segment = segments * segment_length_with_udh;
+		}
 
-    			// Is valid GSM or GSM with extended?
-    			if (is_gsm(c)) {
-    				length += 7;
-    			} else {
-    				if (extended && is_gsm_extended(c)) {
-    					length += 14; // For the ESC char + c
-    				} else {
-    					errors.push( {pos: i, char: c} );
-    				}
-    			}
-    		}
+		var remaining = Math.floor((next_segment - bits) / avg_char);
+		return {
+			bytes: bytes,         // Total bytes
+			remaining: remaining, // Remaining chars
+            est_remaining: remaining, // Remaining chars (estimating size of error chars)
+			segments: segments,   // Total SMS Segments
+			errors: errors        // Array of errored chars
+		};
+    }
 
-    		return segments(length, 7, errors);
-	    }
+    function validate_gsm(text, extended) {
 
-	    function is_ucs2(charCode) {
-	    	return char < 0xFFFF;
-	    }
+    	var length = 0; // Length in bits
+		var errors = [];
+		extended = typeof extended !== 'undefined' ? extended : true;
 
-	    function validate_ucs2(text) {
+		for (var i = 0; i < text.length; i++) {
+			var c = text.charAt(i);
 
-	    	var length = 0; // Length in bits
-    		var errors = [];
+			// Is valid GSM or GSM with extended?
+			if (is_gsm(c)) {
+				length += 7;
+			} else {
+				if (extended && is_gsm_extended(c)) {
+					length += 14; // For the ESC char + c
+				} else {
+					errors.push( {pos: i, char: c} );
+				}
+			}
+		}
 
-    		for (var i = 0; i < text.length; i++) {
-    			var c = text.charCodeAt(i);
-    			if (is_ucs2(c)) {
-    				length += 16;
-    			} else {
-    				errors.push( {pos: i, char: c} );
-    			}
-    		}
+		return segments(length, 7, errors);
+    }
 
-    		return segments(length, 16, errors);
-	    }
+    function validate_ucs2(text) {
 
-	    function is_latin1(charCode) {
-	    	return char < 0xFF;
-	    }
+    	var length = 0; // Length in bits
+		var errors = [];
 
+		for (var i = 0; i < text.length; i++) {
+			var c = text.charCodeAt(i);
+			if (is_ucs2(c)) {
+				length += 16;
+			} else {
+				errors.push( {pos: i, char: c} );
+			}
+		}
 
-	    function validate_latin1(text) {
+		return segments(length, 16, errors);
+    }
 
-	    	var length = 0; // Length in bits
-    		var errors = [];
+    function validate_latin1(text) {
 
-    		for (var i = 0; i < text.length; i++) {
-    			var c = text.charCodeAt(i);
-    			if (is_latin1(c)) {
-    				length += 8;
-    			} else {
-    				errors.push( {pos: i, char: c} );
-    			}
-    		}
+    	var length = 0; // Length in bits
+		var errors = [];
 
-    		return segments(length, 8, errors);
-	    }
+		for (var i = 0; i < text.length; i++) {
+			var c = text.charCodeAt(i);
+			if (is_latin1(c)) {
+				length += 8;
+			} else {
+				errors.push( {pos: i, char: c} );
+			}
+		}
 
-//    			isoLength += 8;
-//    			ucs2Length += 16;
+		return segments(length, 8, errors);
+    }
 
-    	function calculate(text) {
-    		var nonGSM = "ç";
+    var encodings = {
+        gsm : {
+            name: "GSM",
+            avg_size: 7,
+            validator: function(v) { 
+                return validate_gsm(v, false);
+            }
+        },
+        gsm_e : {
+            name: "GSM extended",
+            avg_size: 7,
+            validator: validate_gsm
+        },
+        latin1 : {
+            name: "ISO-8859-1 (latin1)",
+            avg_size: 8,
+            validator: validate_latin1
+        },
+        usc2 : {
+            name: "USC-2",
+            avg_size: 16,
+            validator: validate_ucs2
+        },
+    };
 
-    		// Portico's SMSC accepts 7bit ASCII (in octets)
-    		// AT&T requires we send in ISO Latin1 (8 bits) and they map to 7bit
-    		// mBlox defaults to  Latin 1(ISO-8859-1) - They do NOT accept 7bit encodings
-    		// Syniverse supports GSM 7bit by default, and optionally Latin-1
-    		// Velti GSM 7bit or ISO
-    		// SMSS GSM 7bit
+    root.SMS = {
+        encodings: encodings,
 
+        is_gsm : is_gsm,
+        is_gsm_extended : is_gsm_extended,
+        is_ucs2 : is_ucs2,
+        is_latin1 : is_latin1,
 
-			var gsm = validate_gsm(text, true);
-			//var latin1 = validate_latin1(text);
-			var ucs2 = validate_ucs2(text);
+        segments : segments,
+        validate_gsm : validate_gsm,
+        validate_ucs2 : validate_ucs2,
+        validate_latin1 : validate_latin1,
 
-    		var obj = {
-    			gsm: gsm,
-    			//latin1: latin1,
-    			ucs2: ucs2
-    		}
+        validate : function(encoding, text) {
+            var encoding = this.encodings[encoding];
+            return encoding.validator(text);
+        }
+    };
 
-    		console.log(obj);
-
-    		/*
-			var letter = 'a';
-			var letterInAlfabet = gsm.indexOf(letter) !== -1;
-			*/
-			/*
-			var s = encodeURIComponent('héllo');
-			console.log(s);
-			console.log(unescape(s));
-
-			console.log($('#main').val().length);
-			*/
-    	}
+})(this);
